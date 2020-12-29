d541142e917e8777bdf7a629fb96f14e042e1e18
mathiasperalta <mathias@gmail.com>
2020-12-29 12:20:29 -0300

    se conecta con la base de datos

--- src/Components/page/page.jsx
+++ src/Components/page/page.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-
-
+import 'firebase/firebase-firestore';
+import firebase from 'firebase';
+import '../page/page.css'
 class Page extends Component {
+    state = {
+        content: []
+    }
+    async componentDidMount() {
+        const database = firebase.firestore();
+        await database.collection('Page').doc(this.props.match.params.id).onSnapshot( page => {
+            this.setState({
+                content: page.data()
+            })
+        })
+    }
     render() {
+        console.log(this.state.content);
         return(
-            <p>This is the page component {this.props.match.params.id}</p>
+            <div>
+                <img src={this.state.content.imagen} className="w-100 img-responsive" alt=""/>
+                <div className="container">
+                    <h1 className="pageTitle mt-3">{this.state.content.titulo}</h1>
+                    <p className="pageContent mt-3">{this.state.content.contenido}</p>
+                </div>
+            </div>
         )
     }
 }
