81f3d447673ca763fd4a79920306c3a24fd9564d
mathiasperalta <mathias@gmail.com>
2020-12-29 09:52:08 -0300

    se termina parcialmente el nav

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -8,8 +8,9 @@ class Nav extends Component {
                 <div className="container">
                     <a className="navbar-brand" href="#" id="navbarName">Mathias Peralta</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="currentColor" class="bi bi-list svg" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                        </svg>                    </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto" id="ulList">
                             <li className="nav-item active">
