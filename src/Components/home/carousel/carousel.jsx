cd446bf3b9f3d167d6d78bf903b4e90ec1fa18a9
mathiasperalta <mathias@gmail.com>
2021-01-05 09:54:10 -0300

    se elimina un item del carousel

--- src/Components/home/carousel/carousel.jsx
+++ src/Components/home/carousel/carousel.jsx
@@ -11,7 +11,6 @@ class Carousel extends Component {
                 <ol className="carousel-indicators">
                     <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
                     <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
-                    <li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
                 </ol>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
@@ -29,20 +28,11 @@ class Carousel extends Component {
                     </div>
                     <div className="carousel-item">
                         <Link to="/Page/2">
-                            <img src="https://firebasestorage.googleapis.com/v0/b/vlog-7a01f.appspot.com/o/3318.jpg?alt=media&token=de56e7f0-7d59-46b4-8035-d07623c986f8" className="d-block w-100 carouselImg" alt="..."/>
+                            <img src="https://firebasestorage.googleapis.com/v0/b/vlog-7a01f.appspot.com/o/chrome.jpeg?alt=media&token=54406875-9f26-42db-b193-d6f1a8b8fd2a" className="d-block w-100 carouselImg" alt="..."/>
                         </Link>
                     <div className="carousel-caption d-none d-md-block">
                         <h3 className="carouselTitle" >El coche de Apple llegará como pronto en 2025, siendo 2028 una fecha realista, según Ming-Chi Kuo</h3>
                         <p className="carouselContent">Diversas fuentes fiables hablan de un Apple Car para esta década, pero Kuo llama a la calma diciendo que el mercado es demasiado optimista con él.</p>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                        <Link to="/Page/3">
-                            <img src="https://picsum.photos/1080/400" className="d-block w-100 carouselImg" alt="..."/>
-                        </Link>
-                    <div className="carousel-caption d-none d-md-block">
-                        <h3 className="carouselTitle">WhatsApp dejara de funcionar en estos móviles en 2021: cuáles son y cómo saber si el tuyo se verá afectado</h3>
-                        <p className="carouselContent">WhatsApp dejara de funcionar en estos móviles en 2021: cuáles son y cómo saber si el tuyo se verá afectado</p>
                     </div>
                     </div>
                 </div>
